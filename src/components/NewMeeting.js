489273225b06cc084847b216e85b5955dd4000a6
Rachit Garg <rachitgarg56@gmail.com>
2024-07-12 11:30:50 +0530

    credentials updated

--- src/components/NewMeeting.js
+++ src/components/NewMeeting.js
@@ -8,8 +8,8 @@ const NewMeeting = () => {
 
   
   const apiKey = 'mmhfdzb5evj2'
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiQy0zUE8iLCJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0MtM1BPIiwiaWF0IjoxNzE5NTUzNjM1LCJleHAiOjE3MjAxNTg0NDB9.9yGJOOHnW6roIJYPErQN0Id4jmIU8jgCjB9jCqJmrt0'
-  const userId = 'C-3PO'
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiV2F0dG8iLCJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1dhdHRvIiwiaWF0IjoxNzIwNzYzOTg2LCJleHAiOjE3MjEzNjg3OTF9.kVq2aOTM5JXyeP-UkFiJJGTFeF94JDBc2TYKWIr11nY'
+  const userId = 'Watto'
   // const callId = 'kncnHWBHEoI2'//i can define
   // const callId = copiedCallId !== undefined ? copiedCallId : meetingId;
   const { callId } = useParams(); 
